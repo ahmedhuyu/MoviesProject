156cacf35c2ad3869ea6adb91039a4f9e3c0c22d
ahmedhuyu <ahmed.huyu@gmail.com>
2025-03-14 20:34:38 +0300

    admin page : display list, filter list

--- Movies/MoviesListApp/admin.py
+++ Movies/MoviesListApp/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Movies_Info, Publisher, Contributors, MovieContributor, Review
 
-admin.site.register(Publisher)
-admin.site.register(Movies_Info)
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website', 'email')
+
+class MovieAdmin(admin.ModelAdmin):
+    list_filter = ('publisher',)
+
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Movies_Info, MovieAdmin)
 admin.site.register(Contributors)
 admin.site.register(MovieContributor)
 admin.site.register(Review)
